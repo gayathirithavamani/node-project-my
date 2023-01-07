bc25f7404a5dce76945c14d0655fefe265909a1c
gayathirithavamani <gayat2000@gmail.com>
2023-01-07 21:58:48 +0530

    route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
+import moviesRouter from "./routes/movies.routes.js";
 dotenv.config();
 console.log(process.env.MONGO_URL);
 const app = express();
@@ -20,66 +21,10 @@ app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
 
-app.get("/movies", async function (request, response) {
-  if (request.query.rating) {
-    request.query.rating = +request.query.rating;
-  }
-  console.log(request.query);
-  const movies = await client
-    .db("b40wd")
-    .collection("movies")
-    .find(request.query)
-    .toArray();
-  // console.log(movies);
-  response.send(movies);
-});
+app.use("/movies", moviesRouter);
 
-app.get("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const movie = await client
-    .db("b40wd")
-    .collection("movies")
-    .findOne({ id: id });
-  // const movie = movies.find((mv) => mv.id == id);
-  console.log(movie);
-  movie
-    ? response.send(movie)
-    : response.status(404).send({ message: "movie not found" });
-});
-
-app.post("/movies", async function (request, response) {
-  const data = request.body;
-  console.log(data);
-  const result = await client.db("b40wd").collection("movies").insertMany(data);
-  response.send(result);
-});
-app.delete("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const result = await client
-    .db("b40wd")
-    .collection("movies")
-    .deleteOne({ id: id });
-  // const movie = movies.find((mv) => mv.id == id);
-  console.log(result);
-  result
-    ? response.send(result)
-    : response.status(404).send({ message: "movie not found" });
-});
-
-app.put("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const data = request.body;
-  const result = await client
-    .db("b40wd")
-    .collection("movies")
-    .updateOne({ id: id }, { $set: data });
-
-  // const movie = movies.find((mv) => mv.id == id);
-  console.log(result);
-  result
-    ? response.send({ message: "movies uptated successfully😁" })
-    : response.status(404).send({ message: "movie not found" });
-});
 app.listen(PORT, () =>
   console.log(`The server started in: ${PORT} ✨✨😍😍😘😘❤️❤️✌️✌️`)
 );
+
+export { client };
---
+++ routes/movies.routes.js
@@ -0,0 +1,58 @@
+import express from "express";
+import {
+  getMovies,
+  getMovieById,
+  createMovies,
+  deleteMovieById,
+  updateMovieById,
+} from "../services/movies.service.js";
+const router = express.Router();
+
+router.get("/", async function (request, response) {
+  if (request.query.rating) {
+    request.query.rating = +request.query.rating;
+  }
+  console.log(request.query);
+  const movies = await getMovies(request);
+  // console.log(movies);
+  response.send(movies);
+});
+
+router.get("/:id", async function (request, response) {
+  const { id } = request.params;
+  const movie = await getMovieById(id);
+  // const movie = movies.find((mv) => mv.id == id);
+  console.log(movie);
+  movie
+    ? response.send(movie)
+    : response.status(404).send({ message: "movie not found" });
+});
+
+router.post("/", async function (request, response) {
+  const data = request.body;
+  console.log(data);
+  const result = await createMovies(data);
+  response.send(result);
+});
+router.delete("/:id", async function (request, response) {
+  const { id } = request.params;
+  const result = await deleteMovieById(id);
+  // const movie = movies.find((mv) => mv.id == id);
+  console.log(result);
+  result
+    ? response.send(result)
+    : response.status(404).send({ message: "movie not found" });
+});
+
+router.put("/:id", async function (request, response) {
+  const { id } = request.params;
+  const data = request.body;
+  const result = await updateMovieById(id, data);
+
+  // const movie = movies.find((mv) => mv.id == id);
+  console.log(result);
+  result
+    ? response.send({ message: "movies uptate successfully😁" })
+    : response.status(404).send({ message: "movie not found" });
+});
+export default router;
